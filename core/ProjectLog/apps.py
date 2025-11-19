a7a3e641762478d95f1391f226ac7bad94c31bd4
sinaa.afshar@gmail.com <sinaa.afshar@gmail.com>
2025-11-19 15:26:10 +0330

    git remove cache

--- core/ProjectLog/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProjectlogConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ProjectLog'
-    
-    def ready(self):
-        import ProjectLog.signals  # لینک سیگنال‌ها
--- core/ProjectLog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import audit_log_list, audit_log_detail
-
-urlpatterns = [
-    path('logs/', audit_log_list, name='audit_log_list'),
-    path('logs/<int:log_id>/', audit_log_detail, name='audit_log_detail'),
-]
